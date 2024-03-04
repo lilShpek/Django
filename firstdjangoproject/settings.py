b262f52b715b5276796fe35f89dfe048033c20af
Daniil <kalarash2006@mail.ru>
2024-03-05 00:13:47 +0200

    04.03.1

--- firstdjangoproject/settings.py
+++ firstdjangoproject/settings.py
@@ -151,56 +151,56 @@ MEDIA_ROOT = BASE_DIR / "media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGGING = {
-    # 'version': 1,
-    # 'disable_existing_loggers': False,
-    # 'formatters': {
-    #     'simple': {
-    #         'format': '%(levelname)s %(message)s'
-    #     },
-    #     'verbose': {
-    #         'format': '{levelname} {asctime} {module} {message}',
-    #         'style': '{',
-    #         'datefmt': '%d-%m-%Y %H:%M:%S',  # нужное форматирование даты/времени
-    #     },
-    # },
-    # 'handlers': {
-    #     'console': {
-    #         'class': 'logging.StreamHandler',
-    #         'formatter': 'verbose',
-    #     },
-    #     'file': {
-    #         'class': 'logging.FileHandler',
-    #         'filename': './log/django.log',
-    #         'formatter': 'verbose',  # добавляем нужное нам форматирование
-    #     },
-    # },
-    # 'loggers': {
-    #     'django': {
-    #         'handlers': ['console', 'file'],
-    #         'level': 'INFO',
-    #     },
-    #     'secondapp': {
-    #         'handlers': ['console', 'file'],
-    #         'level': 'DEBUG',
-    #         'propagate': True,
-    #     },
-    # },
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': './log/django.log',
-            'encoding': 'utf-8',
-        },
-    },
-    'loggers': {
-        'secondapp.views': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    },
-}
+# LOGGING = {
+#     # 'version': 1,
+#     # 'disable_existing_loggers': False,
+#     # 'formatters': {
+#     #     'simple': {
+#     #         'format': '%(levelname)s %(message)s'
+#     #     },
+#     #     'verbose': {
+#     #         'format': '{levelname} {asctime} {module} {message}',
+#     #         'style': '{',
+#     #         'datefmt': '%d-%m-%Y %H:%M:%S',  # нужное форматирование даты/времени
+#     #     },
+#     # },
+#     # 'handlers': {
+#     #     'console': {
+#     #         'class': 'logging.StreamHandler',
+#     #         'formatter': 'verbose',
+#     #     },
+#     #     'file': {
+#     #         'class': 'logging.FileHandler',
+#     #         'filename': './log/django.log',
+#     #         'formatter': 'verbose',  # добавляем нужное нам форматирование
+#     #     },
+#     # },
+#     # 'loggers': {
+#     #     'django': {
+#     #         'handlers': ['console', 'file'],
+#     #         'level': 'INFO',
+#     #     },
+#     #     'secondapp': {
+#     #         'handlers': ['console', 'file'],
+#     #         'level': 'DEBUG',
+#     #         'propagate': True,
+#     #     },
+#     # },
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'file': {
+#             'level': 'INFO',
+#             'class': 'logging.FileHandler',
+#             'filename': './log/django.log',
+#             'encoding': 'utf-8',
+#         },
+#     },
+#     'loggers': {
+#         'secondapp.views': {
+#             'handlers': ['file'],
+#             'level': 'INFO',
+#             'propagate': True,
+#         },
+#     },
+# }
